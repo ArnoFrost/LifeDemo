b1a04aa471286c7f51393d88c50f20b39f372e8c
xin.xu <xin.xu@carrobot.com>
2021-03-30 17:38:13 +0800

    WIP: 调整订餐卡片中

--- app/src/main/java/com/arno/demo/life/dinner/DinnerCardView.kt
+++ app/src/main/java/com/arno/demo/life/dinner/DinnerCardView.kt
@@ -3,10 +3,20 @@ package com.arno.demo.life.dinner
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
-import androidx.cardview.widget.CardView
+import android.view.View
+import android.view.ViewOutlineProvider
 
 
-class DinnerCardView(context: Context, attrs: AttributeSet) : CardView(context, attrs) {
+class DinnerCardView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+    init {
+        outlineProvider = object : ViewOutlineProvider() {
+            override fun getOutline(view: View?, outline: Outline?) {
+                outline?.setRoundRect(0, 0, width, height, mCornerSize)
+            }
+        }
+        clipToOutline = true
+    }
 
     private val mPaint: Paint by lazy {
         Paint().apply {
@@ -19,39 +29,84 @@ class DinnerCardView(context: Context, attrs: AttributeSet) : CardView(context, 
     }
 
     private val backGroundRect: Rect by lazy {
-        Rect(0, 0, width, height)
+        Rect(0, 0, measuredWidth, measuredHeight)
+    }
+    private var mCornerSize: Float = 18F
+
+    private val backGroundRectF: RectF by lazy {
+        RectF(0F, 0F, measuredWidth.toFloat(), measuredHeight.toFloat())
+    }
+
+    private var isActive = true
+
+    private fun getStateColor(): Int {
+        return return if (isActive) {
+            Color.parseColor("#3BCF76")
+        } else {
+            Color.GRAY
+        }
     }
 
     private val circleRadius = 50F
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        drawCard(canvas)
+    }
+
+    private val linePathEffect: DashPathEffect by lazy {
+        DashPathEffect(floatArrayOf(8F, 4F), 0F)
+    }
+
+    /**
+     * 绘制背景
+     */
+    private fun drawCard(canvas: Canvas?) {
+        //region 绘制扣图
         //1。 存档
         val saved = canvas?.saveLayer(null, null, Canvas.ALL_SAVE_FLAG)
-        mPaint.color = Color.GREEN
-        canvas?.drawRect(backGroundRect, mPaint)
-
+        mPaint.color = getStateColor()
+//        canvas?.drawRect(backGroundRect, mPaint)
+        canvas?.drawRoundRect(backGroundRectF, mCornerSize, mCornerSize / 2, mPaint)
         //2.叠加 扣除圆形
         mPaint.xfermode = circleXFerMode
         canvas?.drawCircle(circleX(true), circleY(true), circleRadius, mPaint)
         canvas?.drawCircle(circleX(false), circleY(false), circleRadius, mPaint)
-
         //3. 清除
         mPaint.xfermode = null
         canvas?.restoreToCount(saved!!)
+        //endregion
 
+
+        canvas?.apply {
+            mPaint.color = getLineColor()
+            mPaint.strokeWidth = 4F
+            mPaint.pathEffect = linePathEffect
+            drawLine(
+                circleX(true),
+                circleY(true) + circleRadius,
+                circleX(false),
+                circleY(false) - circleRadius,
+                mPaint
+            )
+        }
+
+    }
+
+    private fun getLineColor(): Int {
+        return Color.parseColor("#848887")
     }
 
     private fun circleY(isTop: Boolean): Float {
         return if (isTop) {
             0F
         } else {
-            height.toFloat()
+            measuredHeight.toFloat()
         }
 
     }
 
     private fun circleX(isTop: Boolean): Float {
-        return (width * 0.75).toFloat()
+        return (measuredWidth * 0.75).toFloat()
     }
 
 
