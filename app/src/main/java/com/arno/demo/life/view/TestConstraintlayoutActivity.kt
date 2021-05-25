cc30bb461df557a2685f2f83f4644ac78772a9e5
xin.xu <xin.xu@carrobot.com>
2021-05-25 11:04:55 +0800

    Feature: Add test ConstraintLayout

--- app/src/main/java/com/arno/demo/life/view/TestConstraintlayoutActivity.kt
+++ app/src/main/java/com/arno/demo/life/view/TestConstraintlayoutActivity.kt
@@ -9,4 +9,56 @@ class TestConstraintlayoutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_const)
     }
+
+    /**
+     *
+    StartAreaPressureMultiplier: 10.0
+    StartAreaTemperatureOffset: 20.0
+    NewBaseVisibiltyInnerRadius: 16.5
+    NewBaseVisibiltyRadius: 18
+    PlantSpawnRadius: 100
+    PlantSpawnDensity: 5
+    PlantSpawnAvoidRadius: 5
+    PlantSpawnMax: 70
+    CaveOverrideMaxValue: 0.65
+    CaveOverrideSliverValue: 0.97
+    SubWorldWidth: 256
+    SubWorldHeight: 256
+    ChunkEdgeSize: 32
+    SplitDensityMin: 20
+    SplitDensityMax: 40
+    MediumSplitDensityMin: 15
+    MediumSplitDensityMax: 25
+    HighSplitDensityMin: 10
+    HighSplitDensityMax: 20
+    VeryHighSplitDensityMin: 5
+    VeryHighSplitDensityMax: 10
+    UltraHighSplitDensityMin: 1
+    UltraHighSplitDensityMax: 5
+    SplitTopAndBottomSitesMaxArea: 3000
+    OverworldDensityMin: 600
+    OverworldDensityMax: 700
+    OverworldAvoidRadius: 72.0
+    OverworldSampleBehaviour: UniformHex
+    OverworldRelaxIterations: 3
+    OverworldRelaxEnergyMin: 1.0
+    OverworldMaxNodes: 200
+    DrawWorldBorder: true
+    DrawWorldBorderTop: false
+    WorldBorderThickness: 10
+    WorldBorderRange: 6
+    CloudDaysUntilFirst: 12
+    CloudDaysBetween: 5
+    CloudDaysForNotifyApproaching: 1
+    CloudCountMin: 5
+    CloudCountRange: 4
+    CloudRampMin: 60
+    CloudRampRange: 60
+    CloudSizeMin: 120
+    CloudSizeRange: 60
+    CloudTemperatureMin: 30
+    CloudTemperatureRange: 800
+    CloudMassMin: 2
+    CloudMassRange: 13
+     */
 }
