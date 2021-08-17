589ba5f8e8a3db8929e7013368a437c23191fa43
xuxin14 <xuxin14@staff.sina.com.cn>
2021-08-17 11:11:16 +0800

    Feature: 学习MotionLayout

---
+++ app/src/main/java/com/arno/demo/life/motion/MotionActivity.kt
@@ -0,0 +1,12 @@
+package com.arno.demo.life.motion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.arno.demo.life.R
+
+class MotionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motion)
+    }
+}
